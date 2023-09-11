5cfc6e81a82d2ab4691571f2c0ad73c21f97ebf7
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-09-11 18:24:45 +0300

    Method visibility

---
+++ 19-Method-visibility/SubClass.php
@@ -0,0 +1,14 @@
+<?php
+
+class SubClass extends BaseClass
+{
+    public function getParentProtectedProperty(): string 
+    {
+        return $this->getProtectedProperty();
+    }
+
+    public function getParentPrivateProperty()
+    {
+        return $this->privateProperty; //! Error
+    }
+}
